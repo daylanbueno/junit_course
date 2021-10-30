c13eb23227685b3e330cc62aa6640bbc89399f16
Dailan Bueno <daylansantos@gmail.com>
2021-10-30 20:20:32 -0300

    testing assterions status code for controller

--- src/test/java/io/github/dailan/UnitTesting/controller/HelloWorldControllerTest.java
+++ src/test/java/io/github/dailan/UnitTesting/controller/HelloWorldControllerTest.java
@@ -1,6 +1,5 @@
 package io.github.dailan.UnitTesting.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HelloWorldController.class)
@@ -32,5 +33,19 @@ public class HelloWorldControllerTest {
 
         // VALIDATE RESULT
         assertEquals("Hello World", mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void should_be_return_hello_world_verify_status() throws Exception {
+        // PREPAR TO CALL API
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello-world")
+                .accept(MediaType.APPLICATION_JSON);
+        // EXECUTE
+        MvcResult mvcResult = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andReturn();
+
     }
 }
