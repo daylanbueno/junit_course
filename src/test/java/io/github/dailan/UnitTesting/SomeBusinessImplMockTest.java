c3e66f03d4be76338b8d9aed4dd923ee1c2819e7
Dailan Bueno <daylansantos@gmail.com>
2021-10-30 11:50:38 -0300

    Test refactoring

--- src/test/java/io/github/dailan/UnitTesting/SomeBusinessImplMockTest.java
+++ src/test/java/io/github/dailan/UnitTesting/SomeBusinessImplMockTest.java
@@ -1,67 +1,60 @@
 package io.github.dailan.UnitTesting;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class SomeBusinessImplMockTest {
 
+    private SomeDataService someDataServiceMock;
+    private SomeBusinessImpl business;
+
+    @BeforeEach
+    public void beforeEach() {
+        someDataServiceMock = mock(SomeDataService.class);
+        business = new SomeBusinessImpl(someDataServiceMock);
+    }
+
     @Test
     public void calculateSum_should_be_return_6() {
       SomeBusinessImpl business = new SomeBusinessImpl();
       int actualResult = business.calculateSum(new int [] {1, 2, 3});
-      int expectResult = 6;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(6, actualResult);
     }
 
     @Test
     public void calculateSum_should_be_return_0() {
         SomeBusinessImpl business = new SomeBusinessImpl();
         int actualResult = business.calculateSum(new int [] {0});
-        int expectResult = 0;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(0, actualResult);
     }
 
     @Test
     public void calculateSum_should_be_return_4() {
         SomeBusinessImpl business = new SomeBusinessImpl();
         int actualResult = business.calculateSum(new int [] {1,1,2});
-        int expectResult = 4;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(4, actualResult);
     }
 
     @Test
     public void calculateSum_using_data_service_shuold_be_return_3() {
-        SomeDataService someDataServiceMock = mock(SomeDataService.class);
-        SomeBusinessImpl business = new SomeBusinessImpl(someDataServiceMock);
         when(someDataServiceMock.retrieveAllData()).thenReturn(new int []{ 3});
-
         int actualResult = business.calculateSumUsingServiceDate();
-        int expectResult = 3;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(3,actualResult);
     }
-
 
     @Test
     public void calculateSum_using_data_service_shuold_be_return_empty() {
-        SomeDataService someDataServicemock = mock(SomeDataService.class);
-        when(someDataServicemock.retrieveAllData()).thenReturn(new int[] {});
-        SomeBusinessImpl business = new SomeBusinessImpl(someDataServicemock);
+        when(someDataServiceMock.retrieveAllData()).thenReturn(new int[] {});
         int actualResult = business.calculateSumUsingServiceDate();
-        int expectResult = 0;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(0, actualResult);
     }
-
 
     @Test
     public void calculateSum_using_data_service_shuold_be_return_10() {
-        SomeDataService someDataServiceMock =  mock(SomeDataService.class);
         when(someDataServiceMock.retrieveAllData()).thenReturn(new int[] { 10 });
-
-        SomeBusinessImpl business = new SomeBusinessImpl(someDataServiceMock);
         int actualResult = business.calculateSumUsingServiceDate();
-
-        int expectResult = 10;
-        Assertions.assertEquals(actualResult, expectResult);
+        Assertions.assertEquals(10, actualResult);
     }
 }
