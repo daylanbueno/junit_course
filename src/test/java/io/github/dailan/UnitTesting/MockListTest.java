c64c3380750d398eae3ebcb4aef4992096611c61
Dailan Bueno <daylansantos@gmail.com>
2021-10-30 17:34:07 -0300

    testing for argument capturing

--- src/test/java/io/github/dailan/UnitTesting/MockListTest.java
+++ src/test/java/io/github/dailan/UnitTesting/MockListTest.java
@@ -1,13 +1,15 @@
 package io.github.dailan.UnitTesting;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MockListTest {
@@ -41,5 +43,42 @@ public class MockListTest {
         assertEquals(10, mockLIst.size());
     }
 
+    @Test
+    public  void should_be_verify_call_mathod() {
+        String value = mockLIst.get(0);
+        String value2 = mockLIst.get(0);
+        verify(mockLIst, times(2)).get(0);
+    }
 
+    @Test
+    public  void should_be_verify_call_mathod_parameter_any() {
+        String value = mockLIst.get(0);
+        String value2 = mockLIst.get(anyInt());
+        verify(mockLIst, times(2)).get(0);
+        verify(mockLIst, times(0)).get(10);
+        verify(mockLIst, never()).get(22);
+    }
+
+    @Test
+    public void should_be_argument_capturing() {
+        mockLIst.add("App");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mockLIst).add(captor.capture());
+
+        assertEquals("App", captor.getValue());
+    }
+
+
+    @Test
+    public void should_be_multiple_argument_capturing() {
+        mockLIst.add("App1");
+        mockLIst.add("App2");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mockLIst, times(2)).add(captor.capture());
+
+        assertEquals("App1", captor.getAllValues().get(0));
+        assertEquals("App2", captor.getAllValues().get(1));
+    }
 }
